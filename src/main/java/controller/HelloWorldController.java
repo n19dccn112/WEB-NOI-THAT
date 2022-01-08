727dabe879a255a28adba111e40ea10928223eec
My <n19dccn112@student.ptithcm.edu.vn>
2022-01-08 14:43:02 +0700

    init

---
+++ src/main/java/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/helloMy")
+public class HelloWorldController {
+    @GetMapping()
+    @ResponseBody
+    public String index()
+    {
+        return "MyThui";
+    }
+}
