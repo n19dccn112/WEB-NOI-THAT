23d3a4cbadab036afc3e77b72c8024ee8834c09d
trantoai1 <trantoai.9x@gmail.com>
2022-01-11 19:29:32 +0700

    demo entity

---
+++ src/main/java/d19cqcn02/myvybap/model/entity/Category.java
@@ -0,0 +1,27 @@
+package d19cqcn02.myvybap.model.entity;
+
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long cateId;
+
+    @Column(name = "cate_name")
+    private String categoryName;
+
+    private String description;
+
+//    @Temporal(TemporalType.TIMESTAMP)
+//    @DateTimeFormat(pattern = "dd-MM-yyyy hh:mm:ss")
+//    private Date updateDate;
+
+}
